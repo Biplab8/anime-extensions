632cacbf34478002bd3e412f85bb9567bce25cc1
Biplab8 <64832887+Biplab8@users.noreply.github.com>
2026-07-26 20:28:34 +0530

    Update move-built-apks.py

--- .github/scripts/move-built-apks.py
+++ .github/scripts/move-built-apks.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import shutil
 
-REPO_APK_DIR = Path("repo/apk")
+REPO_APK_DIR = Path("anime-repo/apk")
 
 shutil.rmtree(REPO_APK_DIR, ignore_errors=True)
 REPO_APK_DIR.mkdir(parents=True, exist_ok=True)
